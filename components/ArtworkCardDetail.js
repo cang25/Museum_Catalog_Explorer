0c0a24d11c8633ad3304f0833c80baf642645e94
Christine Ang <christineang1@gmail.com>
2024-10-30 18:00:52 -0400

    formatting artist name

--- components/ArtworkCardDetail.js
+++ components/ArtworkCardDetail.js
@@ -12,44 +12,44 @@ export default function ArtworkCardDetail(props) {
 
   if (!data) return null;
 
-
-    return (
-      <Card style={{ width: "18rem" }}>
-        <Card.Img
-          variant="top"
-          src={
-            data.primaryImage
-              ? data.primaryImage
-              : "https://via.placeholder.com/375x375.png?text=[+Not+Available+]"
-          }
-        />
-        <Card.Body>
-          <Card.Title>{data.title ? data.title : "N/A"}</Card.Title>
-          <Card.Text>
-            <strong>Date: </strong> {data.objectDate ? data.objectDate : "N/A"}
-            <br />
-            <strong>Classification: </strong>{" "}
-            {data.classification ? data.classification : "N/A"}
-            <br />
-            <strong>Art Medium: </strong> {data.medium ? data.medium : "N/A"}
-            <br />
-            <br />
-            <strong>Artist: </strong> {data.artistDisplayName ? data.artistDisplayName: "N/A"}{" "} <a href={data.artistWikidata_URL}
-                target="_blank"
-                rel="noreferrer">wiki</a>
-            <br />
-            <strong>Credit Line:</strong> {data.creditLine}
-            <br />
-            <strong>Dimensions: </strong>
-            {data.dimensions}
-            <br />
-            <br />
-            <Link href={`/artwork/${props.objectID}`} passHref legacyBehavior>
-              <Button variant="primary">{data.objectID}</Button>
-            </Link>
-          </Card.Text>
-        </Card.Body>
-      </Card>
-    );
-
+  return (
+    <Card style={{ width: "18rem" }}>
+      <Card.Img
+        variant="top"
+        src={
+          data.primaryImage
+            ? data.primaryImage
+            : "https://via.placeholder.com/375x375.png?text=[+Not+Available+]"
+        }
+      />
+      <Card.Body>
+        <Card.Title>{data.title ? data.title : "N/A"}</Card.Title>
+        <Card.Text>
+          <strong>Date: </strong> {data.objectDate ? data.objectDate : "N/A"}
+          <br />
+          <strong>Classification: </strong>{" "}
+          {data.classification ? data.classification : "N/A"}
+          <br />
+          <strong>Art Medium: </strong> {data.medium ? data.medium : "N/A"}
+          <br />
+          <br />
+          <strong>Artist: </strong>{" "}
+          {data.artistDisplayName ? data.artistDisplayName : "N/A"}{" "}
+          <a href={data.artistWikidata_URL} target="_blank" rel="noreferrer">
+            wiki
+          </a>
+          <br />
+          <strong>Credit Line:</strong> {data.creditLine}
+          <br />
+          <strong>Dimensions: </strong>
+          {data.dimensions}
+          <br />
+          <br />
+          <Link href={`/artwork/${props.objectID}`} passHref legacyBehavior>
+            <Button variant="primary">{data.objectID}</Button>
+          </Link>
+        </Card.Text>
+      </Card.Body>
+    </Card>
+  );
 }
