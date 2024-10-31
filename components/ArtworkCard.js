7a602b614eea8d0697b8de7d8dc6c9749e725d85
Christine Ang <christineang1@gmail.com>
2024-10-31 18:12:06 -0400

    updated no image photo

--- components/ArtworkCard.js
+++ components/ArtworkCard.js
@@ -21,7 +21,7 @@ export default function ArtworkCard(props) {
           src={
             data.primaryImageSmall
               ? data.primaryImageSmall
-              : "https://via.placeholder.com/375x375.png?text=[+Not+Available+]"
+              : /*"https://via.placeholder.com/375x375.png?text=[+Not+Available+]"*/"https://static.vecteezy.com/system/resources/previews/004/141/669/non_2x/no-photo-or-blank-image-icon-loading-images-or-missing-image-mark-image-not-available-or-image-coming-soon-sign-simple-nature-silhouette-in-frame-isolated-illustration-vector.jpg"
           }
         />
         <Card.Body>
